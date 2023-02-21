1b81a6815c10c3d4ceca1891cf6bfe8ae2f6d1e0
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-02-21 14:05:23 +0100

    inputs

--- inputs.sh
+++ inputs.sh
@@ -2,3 +2,11 @@
 
 read -r -p 'Enter your name: ' name
 echo "your name = $name"
+
+## Special variables####
+##$0-$n, "$*",/ "$@" "$#"
+
+echo "$0" is script name
+echo "$1" first argument
+echo "$*" all arguments
+echo "$#" number of arguments
