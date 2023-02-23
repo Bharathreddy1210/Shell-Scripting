9e9ce676ff33a2d71a863c9794e24fb8f7d99142
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-02-24 00:01:42 +0100

    conditions

--- Conditions.sh
+++ Conditions.sh
@@ -10,7 +10,18 @@
 
 #example
 
-if [ 25 -eq 30 ]
-then
-  echo condition satisfied
+#if [ 25 -ne 30 ]
+#then
+#echo condition satisfied
+#fi
+
+c="xyz"
+if [ '$c' == "xyz" ]; then
+  echo "the equation is true"
 fi
+
+if [ '$b' != "xyz" ]; then
+  echo "the equation is false"
+fi
+
+
