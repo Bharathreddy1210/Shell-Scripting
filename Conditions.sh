87aa6629c90f933f8c078b49575b747dd7e5cd98
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-02-23 23:35:05 +0100

    conditions

---
+++ Conditions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# if conditions are avaliable in three forms
+# simple if
+#Demo
+#if [expression]
+#then
+#command
+#fi
+
+#example
+
+if [ 25 -eq 30 ]
+then
+  echo condition satisfied
+fi
