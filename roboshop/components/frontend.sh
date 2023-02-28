0c73531e4968fa4aecbc90dee13369a8ae676940
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-02-28 21:13:57 +0100

    mongodb

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -24,29 +24,29 @@ if [ "$USER_ID" -ne 0 ]; then
 fi
 Statcheck $?
 
-print " Installing nginx " >>$LOG_FILE
+print " Installing nginx " &>>$LOG_FILE
 yum install nginx -y
 Statcheck $?
 
 print " Downloading nginx "
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$LOG_FILE
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
 Statcheck $?
 
 print " Cleaning up the old nginx content "
-rm -rf /usr/share/nginx/html/* >>$LOG_FILE
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 Statcheck $?
 
 # shellcheck disable=SC2164
 cd /usr/share/nginx/html/
 
 print " download and extract the new content "
-unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* . >>$LOG_FILE && mv static/* . >>$LOG_FILE
+unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* . &>>$LOG_FILE && mv static/* . &>>$LOG_FILE
 Statcheck $?
 
 print " update the system configuration "
-mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 Statcheck $?
 
 print " Starting nginx "
-systemctl restart nginx && systemctl enable nginx >>$LOG_FILE
+systemctl restart nginx && systemctl enable nginx &>>$LOG_FILE
 Statcheck $?
---
+++ roboshop/components/mongodb.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+Statcheck() {
+  if [ "$?" -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+    exit 2
+  fi
+  }
+
+print() {
+  echo -e "...............$1............" >>$LOG_FILE
+  echo -e "\e[36m $1 \e[0m"
+}
+
+USER_ID=$(id -u)
+if [ "$USER_ID" -ne 0 ]; then
+  echo "run as a root user"
+  exit 1
+fi
+
+LOG_FILE=/tmp/roboshop.log
+rm -rf $LOG_FILE
+
+print "downloading mongodb"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
+Statcheck $?
+
+print "Installing mongodb"
+yum install -y mongodb-org &>>$LOG_FILE
+Statcheck $?
+
+print"update mongodb with address"
+sed -i "s/127.0.0.1/0.0.0.0"  /etc/mongod.conf
+Statcheck $?
+
+print "start mongodb"
+systemctl enable mongod &>>$LOG_FILE && systemctl start mongod &>>$LOG_FILE
+Statcheck $?
+
+
