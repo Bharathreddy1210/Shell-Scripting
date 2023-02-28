e09b36a0507923911a4b69abe37aa080858e17e6
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-02-28 16:54:32 +0100

    functions

---
+++ functions.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#Function_name() {
+#commands
+#commands
+#}
+
+Print_statement() {
+  echo function declare at the beginning, before you call the code
+  echo Name assign to the set of commands
+}
+
+Print_statement
