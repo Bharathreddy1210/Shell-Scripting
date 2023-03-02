089825856683161cf8e81848a9fd790d2110d52a
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-03-02 15:21:23 +0100

    catalogue

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,30 +2,30 @@
 
 source components/common.sh
 
-print"configure yum repos"
+print "configure yum repos"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | bash - &>>LOG_FILE
 Statcheck $?
 
-print"install Nodejs"
+print "install Nodejs"
 yum install nodejs -y &>>LOG_FILE
 Statcheck $?
 
-print"add the application user"
+print "add the application user"
 useradd ${APP_USER}
 Statcheck $?
 
-print"download the app component"
+print "download the app component"
 curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>LOG_FILE
 
-print"cleanup the old content"
+print "cleanup the old content"
 rm -rf /home/roboshop/catalogue &>>LOG_FILE
 Statcheck $?
 
-print"extract app content"
+print "extract app content"
 cd /home/roboshop &>>LOG_FILE && unzip /tmp/catalogue.zip &>>LOG_FILE && mv catalogue-main catalogue &>>LOG_FILE
 Statcheck $?
 
-print"Install app dependencies"
+print "Install app dependencies"
 cd /home/roboshop/catalogue &>>LOG_FILE && npm install &>>LOG_FILE
 Statcheck $?
 
