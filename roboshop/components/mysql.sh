c7490edae3794b74aeec7b222ea447f3571d2d89
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-03-03 00:15:39 +0100

    Load Schema mysql

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -25,3 +25,22 @@ if [ $? -ne 0 ]; then
  Statcheck $?
 fi
 
+echo shown plugins | mysql -uroot -pRoboShop@1 2>>$LOG_FILE | grep validate_password
+if [ $? -eq 0 ]; then
+  print "uninstall password validate plugin"
+  echo "uninstall plugin validate_password;" >/tmp/passvalidate.sql
+  mysql --connect-expired-password -uroot -pROboShop@1 </tmp/pass.validate.sql &>>$LOG_FILE
+  Statcheck $?
+fi
+
+print "download schema"
+curl -f -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG_FILE
+Statcheck $?
+
+print "Extract Schema"
+cd /tmp && unzip -o  mysql.zip &>>$LOG_FILE
+Statcheck $?
+
+print "Load Schema"
+cd mysql-main && mysql -u root -pRoboShop@1 <shipping.sql &>>$LOG_FILE
+Statcheck $?
