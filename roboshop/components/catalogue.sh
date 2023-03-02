5ac5227ab3c0a4f1f05c8be3f096fd2d259a64f6
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-03-02 15:32:53 +0100

    replace the roboshop user with the app_user

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -3,7 +3,7 @@
 source components/common.sh
 
 print "configure yum repos"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | bash - &>>LOG_FILE
+curl --silent --location https://rpm.nodesource.com/setup_16.x | bash - &>>${LOG_FILE}
 Statcheck $?
 
 print "install Nodejs"
@@ -11,22 +11,26 @@ yum install nodejs -y &>>LOG_FILE
 Statcheck $?
 
 print "add the application user"
-useradd ${APP_USER}
+id ${APP_USER &>>${LOG_FILE}}
+if [ $? -ne 0 ]; then
+  useradd ${APP_USER} &>>LOG_FILE
+fi
 Statcheck $?
 
+
 print "download the app component"
-curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>LOG_FILE
+curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
 
 print "cleanup the old content"
-rm -rf /home/${APP_USER}/catalogue &>>LOG_FILE
+rm -rf /home/${APP_USER}/catalogue &>>${LOG_FILE}
 Statcheck $?
 
 print "extract app content"
-cd /home/${APP_USER} &>>LOG_FILE && unzip /tmp/catalogue.zip &>>LOG_FILE && mv catalogue-main catalogue &>>LOG_FILE
+cd /home/${APP_USER} &>>${LOG_FILE} && unzip /tmp/catalogue.zip &>>${LOG_FILE} && mv catalogue-main catalogue &>>${LOG_FILE}
 Statcheck $?
 
 print "Install app dependencies"
-cd /home/${APP_USER}/catalogue &>>LOG_FILE && npm install &>>LOG_FILE
+cd /home/${APP_USER}/catalogue &>>LOG_FILE && npm install &>>${LOG_FILE}
 Statcheck $?
 
 
