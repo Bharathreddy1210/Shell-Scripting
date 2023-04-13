f6ae5eefe5183b03ba787260b8c6390b5d792d34
Bharathreddy1210 <bharath1210.g@gmail.com>
2023-04-13 15:09:35 +0200

    create AMI_ID

--- create-ec2.sh
+++ create-ec2.sh
@@ -9,17 +9,16 @@ COMPONENT=$1
 ZONE_ID="Z04815523BC94LQKLB87B"
 
 create_ec2() {
-PRIVATE_IP=$(aws ec2 run-instances \
---image-id ${AMI_ID} \
---instance-type t2.micro \
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
- --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
- --security-group-ids ${SGID} \
- |jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+  PRIVATE_IP=$(aws ec2 run-instances \
+      --image-id ${AMI_ID} \
+      --instance-type t2.micro \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+      --security-group-ids ${SGID} \
+      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
-
   }
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice"  | jq '.Images[].ImageId' | sed -e 's/"//g')
